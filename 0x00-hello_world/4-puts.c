f53ab429d81ec793da28f722d455d4bf37c2d934
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-03-16 13:10:15 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
-int main(void)
-{
-     puts("\"Programming is like building a multilingual puzzle);
-   return (0);
- }
+#include <stdio.h>
+
+int main() {
+    char str[] = "Programming is like building a multilingual puzzle";
+    puts(str);
+    return 0;
+}
