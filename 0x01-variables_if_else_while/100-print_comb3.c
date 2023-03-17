ae7d290e654119a7dd476bf166149ac576ca177a
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-03-17 21:25:41 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 int i, j;
-  for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-  for (j = i + 1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
-  putchar(i + '0');
-  putchar(j + '0');
-  if (i != 8 || j != 9)
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
 {
-  putchar(',');
-  putchar(' ');
- }
- }
- }
+putchar(',');
+putchar(' ');
+}
+}
+}
 return (0);
 }
