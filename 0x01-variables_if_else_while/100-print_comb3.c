07878640732286496945ee770111ea4f81b5014b
SainttAugustine <kevinbwoma@gmail.com>
2023-03-17 10:53:28 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,23 @@
 * Return: always 0
 */
 
-int main(void) 
+int main(void)
 {
 int i, j;
-for (i = 0; i < 10; i++) 
+for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++) 
+for (j = i + 1; j < 10; j++)
 {
-if (i < j) 
+if (i < j)
 {
-printf("%d, %d", i, j);
-} 
-else 
-{
-printf("%d, %d", j, i);
+printf("%d%d, ", i, j);
 }
-if (i != 8 || j != 9) 
+else
 {
-printf(", ");
+printf("%d%d, ", j, i);
 }
 }
 }
+printf("\b\b  \n");
 return (0);
 }
