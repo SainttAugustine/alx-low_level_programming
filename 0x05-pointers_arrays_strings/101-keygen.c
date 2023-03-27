4ba43267ffe6208cc8dfeb1d9eca533a8c973953
SainttAugustine <kevinbwoma@gmail.com>
2023-03-27 02:35:47 -0700

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int i;
+    int sum;
+    char c;
+
+srand(time(NULL));
+
+sum = 0;
+
+while (sum < 2772 - 122)
+{
+c = rand() % 94 + 33;
+putchar(c);
+sum += c;
+}
+
+putchar(2772 - sum);
+
+return (0);
+}
