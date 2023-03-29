4845b49fc06440b453ee4e94d1e463ee80088a7b
SainttAugustine <kevinbwoma@gmail.com>
2023-03-29 12:52:09 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest, and then
+ * adds a terminating null byte. It uses at most n bytes from src. The function
+ * returns a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i] = '\0';
+
+return (dest);
+}
