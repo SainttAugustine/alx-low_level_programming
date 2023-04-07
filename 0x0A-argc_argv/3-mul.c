b9aa177932fbe107db4f694c19b8d588fbb06104
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-04-07 17:01:45 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -48,17 +48,17 @@ int _atoi(char *s)
 
 				digit = -digit;
 
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (n);
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
 }
 /**
  * main - multiplies two numbers
