c9c6979f8ceef8e79cb360cecfc3b5111c732d89
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-04-07 17:23:58 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ f = 0;
 digit = 0;
 while (s[len] != '\0')
 len++;
-while (i < len && f == 0)	
+while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
@@ -32,22 +32,22 @@ break;
 f = 0;
 }
 i++;
-}	
+}
 if (f == 0)
 return (0);
-return (n);	
+return (n);
 }
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 (Success), 1 (Error)	
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
-if (argc < 3 || argc > 3)	
+if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
@@ -56,5 +56,5 @@ num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
 result = num1 * num2;
 printf("%d\n", result);
-return (0);	
+return (0);
 }
