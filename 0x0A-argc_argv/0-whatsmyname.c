c12ed3ff6abebf8b94eaa1568529ae174196d44e
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-04-07 16:42:44 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
 #include "main.h"
 
 
-/*
+/**
  * main - prints the name of the program
+ *
  * @argc: number of arguments
- * @argv: array of arguments
+ *
+ *@argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
