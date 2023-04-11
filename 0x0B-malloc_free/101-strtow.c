85130486f59d3d627fab06d0c4bf48723805ad1d
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-04-11 08:04:37 +0300

    Corrections Task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,22 +3,23 @@
 
 /**
  *  * count_word - helper function to count the number of words in a string
- *   * @s: string to evaluate		
+ *   * @s: string to evaluate
  *    *
- *     * Return: number of words		
+ *     * Return: number of words
  */
 int count_word(char *s)
 {
 		int flag, c, w;
 			flag = 0;
 				w = 0;
-					for (c = 0; s[c] != '\0'; c++)
-							{
-										if (s[c] == ' ')												flag = 0;
-												else if (flag == 0)
-															{
-																			flag = 1;
-																						w++;											}
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+														{
+																		flag = 1;
+																					w++;											}
 		
 													}
 						return (w);
@@ -44,9 +45,8 @@ char **strtow(char *str)
 		return (NULL);
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)	
+if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)	
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -66,8 +66,8 @@ char **strtow(char *str)
 			}
 		}
 		else if (c++ == 0)
-			start = i;	
-	}
+			start = i;
+}
 	matrix[k] = NULL;
 	return (matrix);
 }
