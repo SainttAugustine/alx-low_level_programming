d1a0384e88d71c3f3cde209bc12b82ecc9f1fa04
SainttAugustine <110467576+SainttAugustine@users.noreply.github.com>
2023-04-17 12:35:00 +0300

    Fixing betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
  * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: Name of dog 
+ * @age: Age of dog
+ * @owner: owner of dog 
  *
- * Description: Longer description
  */
 
 struct dog
